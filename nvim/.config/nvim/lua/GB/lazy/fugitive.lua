642b65b50fc4b522d5d79360706282ef02d6f8ca
Gintars <gintars.briedis@gmail.com>
2026-02-04 23:07:15 +0200

    Did the ai refactor to kitty
    Staged kitty.conf

--- nvim/.config/nvim/lua/GB/lazy/fugitive.lua
+++ nvim/.config/nvim/lua/GB/lazy/fugitive.lua
@@ -1,4 +1,13 @@
--- vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 return{
-    "tpope/vim-fugitive"
+    "tpope/vim-fugitive",
+    config = function ()
+      vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", {desc="[G]it [A]dd all"})
+      vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {desc="[G]it [S]tatus"})
+      vim.keymap.set("n", "<leader>gc", ":Git commit -m \"\"<Left>", {desc="[G]it [C]ommit"})
+      vim.keymap.set("n", "<leader>gP", ":Git push<CR>", {desc="[G]it [P]ush"})
+      vim.keymap.set("n", "<leader>gF", ":Git fetch<CR>", {desc="[G]it [F]etch"})
+      vim.keymap.set("n", "<leader>gd", ":Git diff<CR>", {desc="[G]it [D]iff"})
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", {desc="[G]it [B]lame"})
+      vim.keymap.set("n", "<leader>gl", ":Git log<CR>", {desc="[G]it [L]og"})
+    end
 }
