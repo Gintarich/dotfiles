03a532db3cf16a00843a5eb0343465dc4f2126cf
Gintars <gintars.briedis@gmail.com>
2026-03-09 19:02:41 +0200

    Git setup

--- omarchySetup.sh
+++ omarchySetup.sh
@@ -2,6 +2,7 @@
 
 source ./scripts/setup-zsh.sh
 source ./scripts/utils.sh
+source ./scripts/setup_git.sh
 
 # Array of common packages to install
 declare -a PACKAGES=(
@@ -13,6 +14,8 @@ install_packages "${PACKAGES[@]}"
 # Install common packages if not already present
 # sudo pacman -S --noconfirm "${common_packages[@]}"
 
+setup_git
+
 # Check if Neovim is installed
 if command -v nvim &> /dev/null; then
     echo "nvim installed"
@@ -20,9 +23,6 @@ else
     # Install Neovim from the official repositories
     sudo pacman -S --noconfirm neovim
 
-    # Configure Git user information
-    git config --global user.email "gintars.briedis@gmail.com"
-    git config --global user.name "Gintars Briedis"
 fi
 
 # Check if lazygit is installed
@@ -33,16 +33,6 @@ else
     sudo pacman -S --noconfirm lazygit
 fi
 
-
-#Shell stuff
-#You can change zsh env in : nvim /etc/zsh/zshenv
-#add this there ZDOTDIR=~/.config/zsh
-#echo $SHELL
-#Sets shell to zsh if its not set
-if [[ $SHELL != /usr/bin/zsh ]]; then
-    echo "zsh not installed"    
-    chsh -s $(which zsh)
-fi
 
 stow -vt ~ nvim zsh tmux
 
